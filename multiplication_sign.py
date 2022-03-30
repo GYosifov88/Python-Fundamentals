5a5fab91eeb9f7369b0b5b33f013029f10310924
GYosifov88 <gyosifov88@gmail.com>
2022-03-30 17:05:11 +0300

    rewrite different method

--- multiplication_sign.py
+++ multiplication_sign.py
@@ -1,25 +1,35 @@
 def multiplication(first_num, second_num, third_num):
-    positives = 0
-    negatives = 0
-    zeros = 0
-    num_list = list()
-    num_list.append(first_num)
-    num_list.append(second_num)
-    num_list.append(third_num)
-    for num in num_list:
-        if num == 0:
-            zeros += 1
-        elif num > 0:
-            positives += 1
-        elif int(num) < 0:
-            negatives += 1
 
-    if zeros > 0:
+    result = first_num*second_num*third_num
+    if result == 0:
         print('zero')
-    elif negatives % 2 == 0:
-        print('positive')
-    else:
+    elif result < 0:
         print('negative')
+    else:
+        print('positive')
+
+    # ###########code without multiplying the digits as per the task######
+    # positives = 0
+    # negatives = 0
+    # zeros = 0
+    # num_list = list()
+    # num_list.append(first_num)
+    # num_list.append(second_num)
+    # num_list.append(third_num)
+    # for num in num_list:
+    #     if num == 0:
+    #         zeros += 1
+    #     elif num > 0:
+    #         positives += 1
+    #     elif int(num) < 0:
+    #         negatives += 1
+    #
+    # if zeros > 0:
+    #     print('zero')
+    # elif negatives % 2 == 0:
+    #     print('positive')
+    # else:
+    #     print('negative')
 
 
 first_num = int(input())
